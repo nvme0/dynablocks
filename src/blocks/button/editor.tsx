c2f3fc106520b256b43f6a14f7fbef0566936cae
nvme0 <jeff@solutions4theweb.com>
2020-01-24 10:36:24 -0800

    Adds a parent + relationship concept to blocks/button

--- src/blocks/button/editor.tsx
+++ src/blocks/button/editor.tsx
@@ -1,10 +1,11 @@
-import { BlockConfiguration } from "@wordpress/blocks";
+import { BlockConfiguration, BlockEditProps } from "@wordpress/blocks";
 import { Edit } from "./editor/Edit";
 import { settings as baseSettings } from "./editor/settings";
+import { Attributes } from "./editor/attributes";
 
 export { name } from "./editor/settings";
 
 export const settings: BlockConfiguration<any> = {
   ...baseSettings,
-  edit: props => <Edit {...props} />
+  edit: (props: BlockEditProps<Attributes>) => <Edit {...props} />
 };
--- src/blocks/button/editor/Edit.tsx
+++ src/blocks/button/editor/Edit.tsx
@@ -1,35 +1,73 @@
+import { dispatch, select } from "@wordpress/data";
 import { BlockEditProps } from "@wordpress/blocks";
-import { InspectorControls } from "@wordpress/block-editor";
 import ElementControls from "./ElementControls";
 import { Attributes } from "./attributes";
 import { StyledButton } from "../../../common/Components/Bootstrap/Button";
 
-export const Edit = (props: BlockEditProps<Attributes>): JSX.Element => {
+interface EditProps extends BlockEditProps<Attributes> {
+  // extends missing types
+  clientId: string | undefined; // should always have this
+}
+
+const createUpdateFunction = (props: EditProps) => {
+  const { clientId, attributes, setAttributes } = props;
+  const { parentId, relationship } = attributes;
+  let syncWithParent;
+
+  if (parentId && relationship && clientId) {
+    const parentAttributes = select("core/block-editor").getBlockAttributes(
+      parentId
+    );
+
+    if (parentAttributes) {
+      syncWithParent = () => {
+        const { innerBlocks } = parentAttributes;
+        const blockInstance = select("core/block-editor").getBlock(clientId);
+        dispatch("core/block-editor").updateBlockAttributes(parentId, {
+          innerBlocks: {
+            ...innerBlocks,
+            [relationship]: blockInstance
+          }
+        });
+      };
+
+      // sync attributes with parent
+      syncWithParent();
+    }
+  }
+
+  if (!syncWithParent) {
+    return property => value => {
+      setAttributes({ [property]: value });
+    };
+  }
+
+  return property => value => {
+    syncWithParent();
+    setAttributes({ [property]: value });
+  };
+};
+
+export const Edit = (props: EditProps): JSX.Element => {
   const { attributes } = props;
   const { buttonText: text } = attributes;
 
+  const update = createUpdateFunction(props);
   const updateColorPicker = property => value => {
     const { a, b, g, r } = value.rgb;
     const rgbaValue = `rgba(${r},${g},${b},${a})`;
-    props.setAttributes({ [property]: rgbaValue });
-  };
-
-  const update = property => value => {
-    props.setAttributes({ [property]: value });
+    update(property)(rgbaValue);
   };
 
   return (
     <div className="s4tw-dynablocks-button">
-      {/* <ElementControls
+      <ElementControls
         {...{
           ...attributes,
           update,
           updateColorPicker
         }}
-      /> */}
-      {/* <InspectorControls> */}
-      <div>hello, world.</div>
-      {/* </InspectorControls> */}
+      />
       <StyledButton
         {...{
           ...attributes,
