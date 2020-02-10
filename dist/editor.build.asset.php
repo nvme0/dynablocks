4e8e07bbd9ae4a08bb77383636f540660646936b
nvme0 <jeff@solutions4theweb.com>
2020-02-10 07:06:26 -0800

    1.0.1

--- dist/editor.build.asset.php
+++ dist/editor.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7c65bfecb73a0fbe3918d85ef2c00581');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b093f63dcb5cc0340fd64456e517f74b');
--- dist/frontend.build.asset.php
+++ dist/frontend.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e3bc937c059ced276d463b8b25a3254a');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ee9b9728c0b9a2756a488d049e4c1b64');
