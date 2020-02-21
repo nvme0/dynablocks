25d76104b9264c5bc50092a47b6531320f08c7a2
nvme0 <jeff@solutions4theweb.com>
2020-02-21 12:56:03 +1000

    Only sets height to 100% when there is less than 1 innerBlock in blocks/columns-element

--- src/blocks/columns-element/editor/Edit.tsx
+++ src/blocks/columns-element/editor/Edit.tsx
@@ -1,4 +1,5 @@
 import { BlockEditProps } from "@wordpress/blocks";
+import { select } from "@wordpress/data";
 import { Attributes } from "./attributes";
 import ContainerElement from "../Components/ContainerElement";
 
@@ -8,10 +9,21 @@ export interface EditProps extends BlockEditProps<Attributes> {
 }
 
 export const Edit = (props: EditProps): JSX.Element => {
-  const { attributes, className } = props;
+  const { attributes, className, clientId } = props;
+
+  let innerBlockCount = 0;
+  if (clientId) {
+    const blockInstance = select("core/block-editor").getBlock(clientId);
+    if (blockInstance) {
+      innerBlockCount = blockInstance.innerBlocks.length;
+    }
+  }
 
   return (
-    <div className={`s4tw-dynablocks-columns-element`} style={{ padding: 0 }}>
+    <div
+      className="s4tw-dynablocks-columns-element"
+      style={{ padding: 0, height: innerBlockCount > 0 ? undefined : "100%" }}
+    >
       <ContainerElement
         {...{
           className,
