e8b0727fce1c67733cf2adf27c4b78900e32c40b
nvme0 <jeff@solutions4theweb.com>
2020-02-21 14:15:28 +1000

    Fixes error in blocks/columns 'l is undefined' by deleting frontend.tsx

--- src/blocks/columns/frontend.tsx
+++
@@ -1,8 +0,0 @@
-import { Columns } from "./Components/Columns";
-import { Attributes } from "./editor/attributes";
-
-export const className = ".s4tw-dynablocks-columns";
-
-export const EntryPoint = (props: Attributes): JSX.Element => (
-  <Columns {...{ ...props, size: { width: null, height: null } }} />
-);
