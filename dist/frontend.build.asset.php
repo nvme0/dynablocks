75eb5a29a24c09af6a664083a17412c9ba367570
nvme0 <jeff@solutions4theweb.com>
2020-02-26 20:04:59 +1000

    1.1.13

--- dist/frontend.build.asset.php
+++ dist/frontend.build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b3541794551b8c97214787cad303f767');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6b372511a42b6563782e6c4b124ec54b');
