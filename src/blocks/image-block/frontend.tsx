a8d211775cea926973c2559d536cf82c3da7294d
nvme0 <jeff@solutions4theweb.com>
2020-07-26 13:54:45 +1000

    Make image-block & spacer rely on php server render only, and not use react render

--- src/blocks/image-block/frontend.tsx
+++
@@ -1,8 +0,0 @@
-import Divider from "./Components/Divider";
-import { Attributes } from "./editor/attributes";
-
-export const className = ".s4tw-dynablocks-image-block";
-
-export const EntryPoint = (props: Attributes): JSX.Element => (
-  <Divider {...props} />
-);
